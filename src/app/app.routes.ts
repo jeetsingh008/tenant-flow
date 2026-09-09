65e38c66ecfb40f767d29cd304c92b2a2156098d
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2026-09-09 22:17:29 +0530

    Setup basic form for new tenant registration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 
 import { Dashboard } from './components/dashboard/dashboard';
 import { TenantDetail } from './components/tenant-detail/tenant-detail';
+import { AddTenant } from './components/add-tenant/add-tenant';
 
 export const routes: Routes = [
   { path: '', component: Dashboard },
-  { path: 'tenant/:id', component: TenantDetail }
+  { path: 'tenant/:id', component: TenantDetail },
+    { path: 'dashboard', component: Dashboard },
+  { path: 'add-tenant', component: AddTenant}
 ];
