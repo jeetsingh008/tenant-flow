ecd1886215b5e3a43b92891b08df9103ac4a7649
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2026-09-13 10:08:56 +0530

    remove settings and profile page

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { Toast } from './components/toast/toast';
 import { ThemeToggle } from './components/theme-toggle/theme-toggle';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, Toast, ThemeToggle, FormsModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, Toast, ThemeToggle],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('API Kiba');
-  globalSearch = signal('');
 }
